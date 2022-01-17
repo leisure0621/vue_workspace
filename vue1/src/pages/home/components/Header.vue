e0a2dd39f27c1838c7933472f5166afc1ee507bf
leisure0621 <3116118480@qq.com>
2022-01-17 10:29:32 +0800

    20220117-Vue1-項目-首頁開發
    首頁開發所有章節

---
+++ vue1/src/pages/home/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span> <!-- 如果有用阿里巴巴矢量图标库的話，此碼可以在裡面複製 -->
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      輸入城市/景點/遊玩主题
+    </div>
+    <div class="header-right">
+      {{this.city}}
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .header
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
